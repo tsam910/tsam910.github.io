bfa8e19e46830188ffdaa1f3ab4711145a66f4d1
tsam910 <tsam910@gmail.com>
2017-03-05 21:31:20 +0530

    fyve

---
+++ fyve/scripts/main.js
@@ -0,0 +1,78 @@
+var patient = [{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'Last alarm 10 min ago'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : true,
+		message : 'Alarm Active',
+		parameter : 'hr'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'No recent alarms'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'Last alarm 10 min ago'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'Last alarm 10 min ago'
+	}
+}]
+
+$(document).ready(function(){
+	$.tmpl( $('#patientTemplate').html(), patient).appendTo( "#mainContainer .patientsList.urgent" )
+	$.tmpl( $('#patientTemplate').html(), patient).appendTo( "#mainContainer .patientsList.ward1" )
+	$.tmpl( $('#patientTemplate').html(), patient).appendTo( "#mainContainer .patientsList.ward2" )
+})
