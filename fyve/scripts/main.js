04c380060bf31bec0640f3ca7f02c855ffd629b7
tsam910 <tsam910@gmail.com>
2017-03-14 08:27:20 +0530

    finalizaing

--- fyve/scripts/main.js
+++ fyve/scripts/main.js
@@ -71,10 +71,299 @@ var patient = [{
 	}
 }]
 
+var alarms = [{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		},{
+			time : '08:52',
+			duration : '7 min',
+			parameter : 'Heart Rate',
+			average : '71',
+			peak : '97',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : 'Ongoing',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		},{
+			time : '08:52',
+			duration : '7 min',
+			parameter : 'Heart Rate',
+			average : '71',
+			peak : '97',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		},{
+			time : '08:52',
+			duration : '7 min',
+			parameter : 'Heart Rate',
+			average : '71',
+			peak : '97',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'Male',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+}]
+
 $(document).ready(function(){
+	$.tmpl( $('#alarmTemplate').html(), alarms).prependTo( ".patientsContainer .alarms" )
 	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsList.urgent" )
 	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsList.ward1" )
 	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsList.ward2" )
+
+	$('.buttonLogin').bind('click', function(){
+		$('.loginMainContainer').hide();
+	})
 
 	$('.historyOptions .option').bind('click', function(){
 		$('.historyOptions .option').removeClass('active');
@@ -98,5 +387,43 @@ $(document).ready(function(){
 
 	$('.patientContainer').bind('click', function(){
 		$('#mainContainer').addClass('showingDetails');
+	})
+
+	$('.otherOptions .option').bind('click', function(){
+
+		if($(this).hasClass('open')){
+			$(this).removeClass('open');
+		} else {
+			$('.otherOptions .option').removeClass('open');
+			$(this).addClass('open');
+		}
+	})
+
+	$('.tabPatients').bind('click', function(){
+		$('.tabPatients').addClass('active');
+		$('.tabAlarms').removeClass('active');
+		$('.patientsContainer .alarms').hide();
+		$('.patientsContainer .patients').show();
+	})
+
+	$('.tabAlarms').bind('click', function(){
+		$('.tabPatients').removeClass('active');
+		$('.tabAlarms').addClass('active');
+		$('.patientsContainer .alarms').show();
+		$('.patientsContainer .patients').hide();
+	})
+
+	$('.patientDetailsContainer .action.history').bind('click', function(){
+		$('.patientDetailsContainer .action.vitals').removeClass('active');
+		$(this).addClass('active');
+		$('.patientDetailsContainer .patientHistory').show();
+		$('.patientDetailsContainer .patientLiveData').hide();
+	})
+
+	$('.patientDetailsContainer .action.vitals').bind('click', function(){
+		$('.patientDetailsContainer .action.history').removeClass('active');
+		$(this).addClass('active');
+		$('.patientDetailsContainer .patientHistory').hide();
+		$('.patientDetailsContainer .patientLiveData').show();
 	})
 })
